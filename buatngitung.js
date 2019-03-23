0da3eb389b6137f123cad28d4d9cb99fde3ba4b5
Edward Potts <46508197+pottsed@users.noreply.github.com>
2019-03-23 12:00:15 +0700

    Add files via upload

---
+++ buatngitung.js
@@ -0,0 +1,121 @@
+
+
+(function() {
+  "use strict";
+
+  var el = function(element) {
+    if (element.charAt(0) === "#") { 
+      return document.querySelector(element);
+    }
+
+    return document.querySelectorAll(element); 
+  };
+
+  var viewer = el("#viewer"), 
+    equals = el("#equals"), 
+    nums = el(".num"), 
+    ops = el(".ops"), 
+    theNum = "", 
+    oldNum = "", 
+    resultNum, 
+    operator; 
+
+  var setNum = function() {
+    if (resultNum) { 
+      theNum = this.getAttribute("data-num");
+      resultNum = "";
+    } else { 
+      theNum += this.getAttribute("data-num");
+    }
+
+    viewer.innerHTML = theNum; 
+
+  };
+
+  var moveNum = function() {
+    oldNum = theNum;
+    theNum = "";
+    operator = this.getAttribute("data-ops");
+
+    equals.setAttribute("data-result", ""); 
+  };
+
+  
+  var displayNum = function() {
+
+    oldNum = parseFloat(oldNum);
+    theNum = parseFloat(theNum);
+
+    // Perform operation
+    switch (operator) {
+      case "plus":
+        resultNum = oldNum + theNum;
+        break;
+
+      case "minus":
+        resultNum = oldNum - theNum;
+        break;
+
+      case "times":
+        resultNum = oldNum * theNum;
+        break;
+
+      case "divided by":
+        resultNum = oldNum / theNum;
+        break;
+
+        
+      default:
+        resultNum = theNum;
+    }
+
+    
+    if (!isFinite(resultNum)) {
+      if (isNaN(resultNum)) { 
+        resultNum = "You broke it!";
+      } else { 
+        resultNum = "Read tittle above";
+        el('#calculator').classList.add("broken"); 
+      }
+    }
+
+    // Display result, finally!
+    viewer.innerHTML = resultNum;
+    equals.setAttribute("data-result", resultNum);
+
+    oldNum = 0;
+    theNum = resultNum;
+
+  };
+
+  var clearAll = function() {
+    oldNum = "";
+    theNum = "";
+    viewer.innerHTML = "0";
+    equals.setAttribute("data-result", resultNum);
+  };
+
+  /* The click events */
+
+  // Add click event to numbers
+  for (var i = 0, l = nums.length; i < l; i++) {
+    nums[i].onclick = setNum;
+  }
+
+  // Add click event to operators
+  for (var i = 0, l = ops.length; i < l; i++) {
+    ops[i].onclick = moveNum;
+  }
+
+  // Add click event to equal sign
+  equals.onclick = displayNum;
+
+  // Add click event to clear button
+  el("#clear").onclick = clearAll;
+
+  // Add click event to reset button
+  el("#reset").onclick = function() {
+    window.location = window.location;
+  };
+
+}());
